1f53818662a169ff94f9e0ce283d90d6e1c4d8b0
Ales Cervenka <ales.cervenka@inzata.com>
2020-11-15 22:08:18 +0100

    Return the 404 Not found for the unimplemented methods.

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -50,6 +50,13 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Throwable $exception)
     {
+        if ($exception instanceof MethodNotAllowedHttpException) {
+            return response()->json([
+                'message' => 'Resource Not Found',
+                'status' => false
+            ], 404
+            );
+        }
         return parent::render($request, $exception);
     }
 }
